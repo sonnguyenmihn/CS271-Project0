4fd5d2987c3b939ba3e666a9f70c810b43453df9
trinitrono <meckel_t1@denison.edu>
2024-09-17 15:28:56 -0400

    Update DoublyLinkedList.cpp
    added prepend

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -4,8 +4,35 @@
 using namespace std;
 
 template <class T>
-DoublyLinkedList<T>::DoublyLinkedList() {
+DoublyLinkedList<T>::DoublyLinkedList(void) 
+{
     head = NULL;
     tail = NULL;
     length = 0;
 }
+
+template <class T>
+void DoublyLinkedList<T>::prepend(const &item)
+{
+    // Create new node
+    Node *newNode = new Node;
+    newNode->val = item;
+    newNode->prev = NULL; // New node will be head, so no previous node
+    newNode->next = head; // New node next is current head
+
+    // Check if the list is empty
+    if ( head == NULL )
+    {
+        tail = newNode; // if list is empty, both tail & head should point to new node
+    }
+    else:
+    {
+        head->prev = newNode; // Update prev head to new node
+    }
+
+    head = newNode; // New node becomes head of list
+    length++; 
+}
+
+
+
