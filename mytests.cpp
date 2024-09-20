07cc2e52d30c330de96de5f89ec8d222c78a7a0f
trinitrono <meckel_t1@denison.edu>
2024-09-20 17:31:21 -0400

    Update mytests.cpp

--- mytests.cpp
+++ mytests.cpp
@@ -3,27 +3,31 @@
 
 using namespace std;
 
-int main() 
+// function to test the constructor and initial list
+void testConstructor(DoublyLinkedList<int>& list) 
 {
-    // create an empty doubly linked
-    DoublyLinkedList<int> list;
-
-    // test default constructor
     cout << "Initial list length: " << list.length() << endl;
+}
 
-    // test prepend function
+// function to test the prepend function
+void testPrepend(DoublyLinkedList<int>& list) 
+{
     cout << "Prepending values to the list.." << endl;
     list.prepend(10);
     list.prepend(20);
     list.prepend(30);
-
-    // test length function
     cout << "List length after prepends: " << list.length() << endl;
+}
 
-    // test overloaded << operator
-    cout << "Cureent list: " << list << endl;
+// function to test the << operator
+void testPrintList(const DoublyLinkedList<int>& list) 
+{
+    cout << "Current list: " << list << endl;
+}
 
-    // test remove function
+// function to test the remove function and handle exceptions
+void testRemove(DoublyLinkedList<int>& list) 
+{
     try 
     {
         cout << "Removing element at index 1 ..." << endl;
@@ -31,7 +35,7 @@ int main()
         cout << "List after removal: " << list << endl;
         cout << "List length after removal: " << list.length() << endl;
 
-        // try remove head & tail
+        // remove head and tail
         cout << "Removing head ( index 0 ) ..." << endl;
         list.remove(0);
         cout << "List after removing head: " << list << endl;
@@ -43,11 +47,23 @@ int main()
         // try out-of-range removal
         cout <<  "Try out-of-range removal (index 5)..." << endl;
         list.remove(5);
-            
     }
-
-    catch (const out_of_range &e)
-    {
+    catch (const out_of_range& e) {
         cout << "Exception caught: " << e.what() << endl;
     }
+}
+
+
+int main() 
+{
+    // create an empty doubly linked list
+    DoublyLinkedList<int> list;
+
+    // test the functions
+    testConstructor(list);
+    testPrepend(list);
+    testPrintList(list);
+    testRemove(list);
+
+    return 0;  
 }
